aa1e1906d143990e82c86cddce75c1ed5b753a61
Nikhil Verma <nikhilvermaanilsdt123@gmail.com>
2026-06-05 09:15:52 +0530

    Add files via upload

---
+++ code185.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Function to perform insertion sort
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+
+    for (i = 1; i < n; i++) {
+        key = arr[i];        // Element to be inserted
+        j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;   // Place key at correct position
+    }
+}
+
+// Function to print array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Main function
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
