86700633291979a7498f68ada8c78f78f6631b7a
Nikhil Verma <nikhilvermaanilsdt123@gmail.com>
2026-07-11 09:33:43 +0530

    Add files via upload

---
+++ code199.c
@@ -0,0 +1,93 @@
+// Count Smaller Elements on Right Side using Merge Sort
+// VS Code Compatible C Program
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int value;
+    int index;
+} Pair;
+
+void merge(Pair arr[], int left, int mid, int right, int count[]) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    Pair *L = (Pair *)malloc(n1 * sizeof(Pair));
+    Pair *R = (Pair *)malloc(n2 * sizeof(Pair));
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    int rightCount = 0;
+
+    while (i < n1 && j < n2) {
+        if (L[i].value <= R[j].value) {
+            count[L[i].index] += rightCount;
+            arr[k++] = L[i++];
+        } else {
+            rightCount++;
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < n1) {
+        count[L[i].index] += rightCount;
+        arr[k++] = L[i++];
+    }
+
+    while (j < n2) {
+        arr[k++] = R[j++];
+    }
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(Pair arr[], int left, int right, int count[]) {
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+
+    mergeSort(arr, left, mid, count);
+    mergeSort(arr, mid + 1, right, count);
+
+    merge(arr, left, mid, right, count);
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    Pair arr[n];
+    int count[n];
+
+    printf("Enter elements:\n");
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+        arr[i].value = nums[i];
+        arr[i].index = i;
+        count[i] = 0;
+    }
+
+    mergeSort(arr, 0, n - 1, count);
+
+    printf("Count of smaller elements on right side:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", count[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
